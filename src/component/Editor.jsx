488e3594f121833896e177b77e0dbc8b6efd2c5b
Precious-Macaulay <preciousmacaulay001@gmail.com>
2022-05-03 16:57:24 +0100

    features update

---
+++ src/component/Editor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Editor() {
+  return (
+    <div>
+      <h3>Editor</h3>
+      <textarea id="editor" class="editor" rows="10" cols={100} ></textarea>
+    </div>
+  );
+}
+
+export default Editor;
---
+++ src/component/Previewer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Previewer() {
+  return (
+    <div>
+      <h3>Previewer</h3>
+      <div id="preview"></div>
+    </div>
+  );
+}
+
+export default Previewer;
